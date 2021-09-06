49964909aea0683a36ffd11a199645eb3545db36
Mikko Pääkkönen <mikko@live.savonia.fi>
2021-09-06 12:38:56 +0300

    initial version

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace net_console_app_base
+{
+    class Options
+    {
+        [Option('i', "input", Required = false, HelpText = "Help for input")]
+        public string Input { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Help for output")]
+        public string OutputFile { get; set; }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var result = await CommandLine.Parser.Default.ParseArguments<Options>(args)
+              .WithParsedAsync(RunOptions);
+            result.WithNotParsed(HandleParseError);        
+        }
+
+        /// <summary>
+        /// The code logic that can use the arguments
+        /// </summary>
+        /// <param name="opts">The arguments parsed from CLI</param>
+        /// <returns></returns>
+        static async Task RunOptions(Options opts)
+        {
+            // check if valid arguments were prodived...
+            if (string.IsNullOrEmpty(opts.Input))
+            {
+                // do something... e.g. set default 
+            }
+
+            // rest of logic...
+        }
+
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
+            Console.WriteLine("Write some usefull info about the app to the user...");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # net-console-app-base
+
 A template for .NET console app with command line arguments
+
+Uses https://www.nuget.org/packages/CommandLineParser/ for command line argument parsing.
+
+Help via https://github.com/commandlineparser/commandline
