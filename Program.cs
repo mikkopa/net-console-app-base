a6582b8ac4a269491f1681f2d35eca27ad57eaa9
Mikko Pääkkönen <mikko@live.savonia.fi>
2021-11-11 11:57:28 +0200

    Migrated template to .NET 6

--- Program.cs
+++ Program.cs
@@ -3,46 +3,44 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using CommandLine;
 
-namespace net_console_app_base
+namespace net_console_app_base;
+class Options
 {
-    class Options
-    {
-        [Option('i', "input", Required = false, HelpText = "Help for input")]
-        public string Input { get; set; }
+    [Option('i', "input", Required = false, HelpText = "Help for input")]
+    public string Input { get; set; }
 
-        [Option('o', "output", Required = true, HelpText = "Help for output")]
-        public string OutputFile { get; set; }
+    [Option('o', "output", Required = true, HelpText = "Help for output")]
+    public string OutputFile { get; set; }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var result = await CommandLine.Parser.Default.ParseArguments<Options>(args)
+          .WithParsedAsync(RunOptions);
+        result.WithNotParsed(HandleParseError);
     }
 
-    class Program
+    /// <summary>
+    /// The code logic that can use the arguments
+    /// </summary>
+    /// <param name="opts">The arguments parsed from CLI</param>
+    /// <returns></returns>
+    static async Task RunOptions(Options opts)
     {
-        static async Task Main(string[] args)
+        // check if valid arguments were prodived...
+        if (string.IsNullOrEmpty(opts.Input))
         {
-            var result = await CommandLine.Parser.Default.ParseArguments<Options>(args)
-              .WithParsedAsync(RunOptions);
-            result.WithNotParsed(HandleParseError);        
+            // do something... e.g. set default 
         }
 
-        /// <summary>
-        /// The code logic that can use the arguments
-        /// </summary>
-        /// <param name="opts">The arguments parsed from CLI</param>
-        /// <returns></returns>
-        static async Task RunOptions(Options opts)
-        {
-            // check if valid arguments were prodived...
-            if (string.IsNullOrEmpty(opts.Input))
-            {
-                // do something... e.g. set default 
-            }
+        // rest of logic...
+    }
 
-            // rest of logic...
-        }
-
-        static void HandleParseError(IEnumerable<Error> errs)
-        {
-            //handle errors
-            Console.WriteLine("Write some usefull info about the app to the user...");
-        }
+    static void HandleParseError(IEnumerable<Error> errs)
+    {
+        //handle errors
+        Console.WriteLine("Write some usefull info about the app to the user...");
     }
 }
